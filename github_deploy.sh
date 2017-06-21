2f087b859085a935f02fd419eac42bdf75d81317
Generoso Martello <generoso@martello.com>
2017-06-21 20:32:55 +0200

    testing automated API docs push to gh-pages

--- github_deploy.sh
+++ github_deploy.sh
@@ -36,8 +36,8 @@ doCompile
 # Now let's go have some fun with the cloned repo
 cp -rf _docs/* out/
 cd out
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config --global user.name "Travis CI"
+git config --global user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 #if git diff --quiet; then
