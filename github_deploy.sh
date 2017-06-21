eaffa833acf5574116d4e8445eb14f1fe707345e
Generoso Martello <generoso@martello.com>
2017-06-21 20:26:07 +0200

    testing automated API docs push to gh-pages

--- github_deploy.sh
+++ github_deploy.sh
@@ -10,9 +10,9 @@ function doCompile {
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-#    echo "Skipping deploy; just doing a build."
-#    doCompile
-#    exit 0
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
 fi
 
 # Save some useful information
