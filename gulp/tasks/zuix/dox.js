3044aa3a86419b4c48103d25b280622fb0f08ed7
Generoso Martello <generoso@martello.com>
2017-06-21 17:29:01 +0200

    testing automated API docs push to gh-pages

--- gulp/tasks/zuix/dox.js
+++ gulp/tasks/zuix/dox.js
@@ -6,37 +6,37 @@ module.exports = function () {
         new Promise(function(resolve, reject) {
             gulp.src('./src/js/zuix/Zuix.js')
                 .pipe(dox({ raw: true }))
-                .pipe(gulp.dest('./docs/content/api/data'))
+                .pipe(gulp.dest('./_docs/content/api/data'))
                 .on('end', resolve);
         }),
         new Promise(function(resolve, reject) {
             gulp.src('./src/js/zuix/ContextOptions.js')
                 .pipe(dox({ raw: true }))
-                .pipe(gulp.dest('./docs/content/api/data'))
+                .pipe(gulp.dest('./_docs/content/api/data'))
                 .on('end', resolve);
         }),
         new Promise(function(resolve, reject) {
             gulp.src('./src/js/zuix/ComponentContext.js')
                 .pipe(dox({ raw: true }))
-                .pipe(gulp.dest('./docs/content/api/data'))
+                .pipe(gulp.dest('./_docs/content/api/data'))
                 .on('end', resolve);
         }),
         new Promise(function(resolve, reject) {
             gulp.src('./src/js/zuix/ContextController.js')
                 .pipe(dox({ raw: true }))
-                .pipe(gulp.dest('./docs/content/api/data'))
+                .pipe(gulp.dest('./_docs/content/api/data'))
                 .on('end', resolve);
         }),
         new Promise(function(resolve, reject) {
             gulp.src('./src/js/helpers/ZxQuery.js')
                 .pipe(dox({ raw: true }))
-                .pipe(gulp.dest('./docs/content/api/data'))
+                .pipe(gulp.dest('./_docs/content/api/data'))
                 .on('end', resolve);
         }),
         new Promise(function(resolve, reject) {
             gulp.src('./src/js/localizer/Localizer.js')
                 .pipe(dox({ raw: true }))
-                .pipe(gulp.dest('./docs/content/api/data'))
+                .pipe(gulp.dest('./_docs/content/api/data'))
                 .on('end', resolve);
         })
     ]).then(function () {
