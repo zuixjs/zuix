7cbf77dcc259bf9f4ffc05b07ed6f1bd0bfbb580
Generoso Martello <generoso.martello@gmail.com>
2018-03-27 00:30:24 +0200

    ZUIX v0.4.9-26

--- dist/js/zuix.js
+++ dist/js/zuix.js
@@ -1,4 +1,4 @@
-/* ZUIX v0.4.9-25 18.03.21 00:05:42 */
+/* ZUIX v0.4.9-26 18.03.27 00:30:24 */
 
 /** @typedef {Zuix} window.zuix */!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.zuix=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 /*
@@ -642,7 +642,12 @@ function ZxQuery(element) {
 
     if (element instanceof ZxQuery)
         return element;
-    else if (element instanceof HTMLCollection || element instanceof NodeList || Array.isArray(element))
+    else if (element instanceof HTMLCollection || element instanceof NodeList) {
+        var list = this._selection = [];
+        z$.each(element, function (i,el) {
+            list.push(el);
+        });
+    } else if (Array.isArray(element))
         this._selection = element;
     else if (element === window || element instanceof HTMLElement || element instanceof Node)
         this._selection = [element];
