7f0f389ab200b0d56a89aeca45e3a5d77fc88beb
Generoso Martello <generoso.martello@gmail.com>
2018-06-02 21:57:54 +0200

    fix API loader

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 zuix.store("config", {
     "title": "zUIx Web Starter application.",
-    "googleSiteId": "UA-123-456",
+    "googleSiteId": "UA-92520304-1",
     "resourcePath": "./app/",
     "libraryPath": "https://genielabs.github.io/zkit/lib"
 });
