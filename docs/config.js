6f1d670c25a23afe1b4af03581460d862b5c4cb2
Generoso Martello <generoso.martello@gmail.com>
2018-06-02 10:40:54 +0200

    fix resource path

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 zuix.store("config", {
     "title": "zUIx Web Starter application.",
     "googleSiteId": "UA-123-456",
-    "resourcePath": "/app/",
+    "resourcePath": "./app/",
     "libraryPath": "https://genielabs.github.io/zkit/lib"
 });
