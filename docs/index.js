3952ba8ca0ff5eb48f132697f1f46ca28ceffd43
Generoso Martello <generoso.martello@gmail.com>
2018-06-14 19:17:48 +0200

    disabled website splash screen

--- docs/index.js
+++ docs/index.js
@@ -89,7 +89,14 @@ zuix.using('component', '@lib/extensions/animate_css', function(res, ctx){
 
 const loaderMessage = zuix.field('loaderMessage');
 const mainPage = zuix.field('main').hide();
-let splashScreen = zuix.field('splashScreen').show();
+let splashScreen = zuix.field('splashScreen');
+if (!isSplashEnabled()) {
+    splashScreen.hide();
+    splashScreen = null;
+    mainPage.show();
+} else {
+    splashScreen.show();
+}
 let revealTimeout = null;
 // reference to homepage's cover and features block (used for change-page animation)
 let coverBlock = null;
@@ -281,6 +288,20 @@ function checkMenuVisibility() {
             zxHeader.hidden = true;
         });
     }
+}
+
+function isSplashEnabled() {
+// eslint-disable-next-line max-len
+    let enabled = true;
+    // Check if client is a crawler/bot
+    const botPattern = '(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)';
+    const re = new RegExp(botPattern, 'i');
+    const userAgent = navigator.userAgent;
+    enabled = enabled && !re.test(userAgent);
+    // TODO: splash screen is disabled by default
+    // TODO: should be enabled when running in "dynamic" mode instead of "static"
+    enabled = false;
+    return enabled;
 }
 
 // PagedView `page:change` behavior handler
