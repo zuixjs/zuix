8832f8076565f54f40205e7e62a057db150bc1f4
Generoso Martello <generoso.martello@gmail.com>
2018-06-17 02:03:57 +0200

    updated website

--- docs/config.js
+++ docs/config.js
@@ -2,5 +2,5 @@ zuix.store("config", {
     "title": "zUIx Web Starter application.",
     "googleSiteId": "UA-92520304-1",
     "resourcePath": "./app/",
-    "libraryPath": "http://192.168.2.104:8081/lib"
+    "libraryPath": "https://genielabs.github.io/zkit/lib"
 });
