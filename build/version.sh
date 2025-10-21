6397cef8d6634884129cab954bd88056a7a9d4ae
gene <generoso.martello@gmail.com>
2025-10-21 02:35:37 +0200

    fix(release): correct preversion script to use command-line argument

--- build/version.sh
+++ build/version.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 set -e
-NEW_VERSION=$npm_package_version
+
+NEW_VERSION=$1
+
+if [[ $NEW_VERSION == v* ]]; then
+  NEW_VERSION=${NEW_VERSION:1}
+fi
+
 PACKAGE_NAME_REGEX="\\[zuix\\.js"
 PACKAGE_NAME="[zuix.js"
+
 echo "Updating README.md for new version v${NEW_VERSION}..."
+
 sed -i.bak "s/${PACKAGE_NAME_REGEX} v[0-9]*\\.[0-9]*\\.[0-9]*/${PACKAGE_NAME} v${NEW_VERSION}/g" ./README.md
 rm ./README.md.bak
+
 git add ./README.md
